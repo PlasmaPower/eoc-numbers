a285d86b282e206d179e8c3eb839b802ce8bbaa6
Lee Bousfield <ljbousfield@gmail.com>
2016-02-07 22:27:16 -0700

    Fixed updateNumbers.sh (for real this time)

--- updateNumbers.sh
+++ updateNumbers.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 node pullNumbers.js
-if ! git diff-index --quiet HEAD json; then
-  git add json
-  git commit -m 'Updated numbers'
-  GIT_SSH="./sshWrapper.sh" git push
-fi
+git add json
+git commit -m 'Updated numbers'
+GIT_SSH="./sshWrapper.sh" git push
